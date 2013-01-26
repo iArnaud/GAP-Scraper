45ebc453932add9b60f588cc1df6597a49965ccd
Kulpreet Chilana <contact@kulpreet.com>
2013-01-26 17:36:24 -0500

    Made the page look pretty. Also added top five sites and removed crappy sites from the DB.

--- sparklines/main.rb
+++ sparklines/main.rb
@@ -44,3 +44,16 @@ def impact_for_id(id)
     db.close if db
   end
 end
+
+def top5_for_id(id)
+  begin
+    db = SQLite3::Database.open "gap_data.db"
+    rs = db.execute "SELECT url FROM sites JOIN (SELECT \"website id\" FROM impact WHERE \"country id\" = ? ORDER BY users DESC LIMIT 0,5) WHERE \"website id\" = sites.id", id 
+    return rs.collect {|x| x.first}
+  rescue SQLite3::Exception => e 
+    puts "Exception occured"
+    puts e
+  ensure
+    db.close if db
+  end
+end
